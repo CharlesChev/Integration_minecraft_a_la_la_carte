29830fd471764c7b70f81c4278c7808e1b7452b6
cchevreuil <charles.chevreuil@ign.fr>
2020-11-29 16:23:15 +0100

    deuxième commit

--- js/main.js
+++ js/main.js
@@ -164,8 +164,8 @@ function setDefaultValues() {
     data_osm_height = 0;
 }
 
- // Load website parameters (API key, projections, URLs, zoom level ...) into config var
- $.getJSON('./inc/config.json', {_: new Date().getTime()}, function(data) {
+// Load website parameters (API key, projections, URLs, zoom level ...) into config var
+$.getJSON('./inc/config.json', {_: new Date().getTime()}, function(data) {
     config = data;
     initDragAbility(); // Openlayers
     getRemainingGenerations();
@@ -175,33 +175,33 @@ function setDefaultValues() {
     }, 4000);
     //$('.generations_per_day').text(config.generations_per_day);
 
-    map = new ol.Map({
-        target: 'map_container',
-        controls: [
-            new ol.control.Zoom(),
-            new ol.control.ScaleLine(),
-            new ol.control.MousePosition({
-                coordinateFormat: ol.coordinate.createStringXY(4),
-                projection: 'EPSG:4326',
-                className: 'custom-mouse-position',
-                target: document.getElementById('mouse_position'),
-                undefinedHTML: '&nbsp;'
-            })
-        ],
-        view: new ol.View({
-                center: ol.proj.transform(config.default_location, 'EPSG:4326', 'EPSG:3857'),
-                zoom: config.default_zoom_level,
-                minZoom: 4,
-                maxZoom: 18
-        }),
-        interactions: ol.interaction.defaults().extend([new app.Drag()])
-    });
-    initMap();
-    
-    // Set-up coherence controls for fields (mail, captcha etc) + reverse geocoding event for address field (findPlace function)
-    $('#address_field').keyup(function() {
-        findPlace($('#address_field').val());
-    });
+map = new ol.Map({
+    target: 'map_container',
+    controls: [
+        new ol.control.Zoom(),
+        new ol.control.ScaleLine(),
+        new ol.control.MousePosition({
+            coordinateFormat: ol.coordinate.createStringXY(4),
+            projection: 'EPSG:4326',
+            className: 'custom-mouse-position',
+            target: document.getElementById('mouse_position'),
+            undefinedHTML: '&nbsp;'
+        })
+    ],
+    view: new ol.View({
+            center: ol.proj.transform(config.default_location, 'EPSG:4326', 'EPSG:3857'),
+            zoom: config.default_zoom_level,
+            minZoom: 4,
+            maxZoom: 18
+    }),
+    interactions: ol.interaction.defaults().extend([new app.Drag()])
+});
+initMap();
+
+// Set-up coherence controls for fields (mail, captcha etc) + reverse geocoding event for address field (findPlace function)
+$('#address_field').keyup(function() {
+    findPlace($('#address_field').val());
+});
 });
 
 
@@ -387,43 +387,6 @@ $("#radio_moins15").on("click", function(){
     $('#derniere_etape_content').css('height','560px');
 });
 
-/*/////////////////////////////////////////////*/
-/*a terminé*/
-/*afficheage bulle sliders*/
-$('#echelleRange').on('mouseenter',function(){
-    $('#slider_value').css('display','block');
-});
-$('#echelleRange').on('mouseleave',function(){
-    $('#slider_value').css('display','none');
-});
-
-
-$("#echelleRange").on("change", function(){
-    var val = $('#echelleRange').val();
-    $("#slider_value").text(val);
-
-    var left = $('#echelleRange')['0']['offsetLeft'];
-    var top = $('#echelleRange')['0']['offsetTop'];
-    var max = $('#echelleRange').attr('max');
-    var min = $('#echelleRange').attr('min');
-    var step = $('#echelleRange').attr('step');
-
-    topPos = top - 10;
-    console.log(topPos);
-    topPos = topPos.toString();
-    console.log(topPos);
-
-    $('#slider_value').css('top',topPos+'px');
-
-    console.log(left);
-    console.log(top);
-    console.log(min);
-    console.log(max);
-    console.log(step);
-    console.log($('#echelleRange'));
-
-});
-
 
 function errorModal(message ="Ceci est un message d'erreur") {
     $('#error_modal').html('<div id = error_content><div class = "modal_content_wrapper"><div class = "error"><p>'+message+'</p></div> <div class = "modal_button"><button type="button" id ="button_error"class= "button_optional">Fermer</button></div></div></div>');
@@ -433,6 +396,10 @@ function errorModal(message ="Ceci est un message d'erreur") {
     });
 };  
 
+/*event a sup après modal mis en responsive
+$('#testfonc').on("mouseenter",function () {
+    errorModal();
+});*/
 
 /*toogle ui_container/map en responsive*/
 if($(window).width() <= 768){
@@ -471,4 +438,7 @@ $(window).resize(function(){
     }
 });
 
-                
+
+
+
+
